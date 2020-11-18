1736928504401a738b56f5b77ea1ebc1956e5ef1
glauroqj <glauro.juliani@hotmail.com>
2020-11-18 20:17:36 -0300

    add

---
+++ src/credentials.js
@@ -0,0 +1,11 @@
+const config = {
+  apiKey: process.env.REACT_APP_API_KEY_FIREBASE,
+  authDomain: "planning-poker-3326c.firebaseapp.com",
+  databaseURL: "https://planning-poker-3326c.firebaseio.com",
+  projectId: "planning-poker-3326c",
+  storageBucket: "planning-poker-3326c.appspot.com",
+  messagingSenderId: "873603982878",
+  appId: "1:873603982878:web:771bcf8f7569c7b31f300f"
+}
+
+export default config

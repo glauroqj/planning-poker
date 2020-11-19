c639ac6870f445ca641eb0bdcce7b75f9bf2329d
glauroqj <glauro.juliani@hotmail.com>
2020-11-19 15:08:36 -0300

    fix: envs

--- src/credentials.js
+++ src/credentials.js
@@ -1,8 +1,8 @@
 const config = {
-  apiKey: process.env.REACT_APP_API_KEY_FIREBASE,
-  authDomain: "planning-poker-3326c.firebaseapp.com",
-  databaseURL: "https://planning-poker-3326c.firebaseio.com",
-  projectId: "planning-poker-3326c",
+  apiKey: process.env.REACT_APP_KEY_FIREBASE,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DB_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: "planning-poker-3326c.appspot.com",
   messagingSenderId: "873603982878",
   appId: "1:873603982878:web:771bcf8f7569c7b31f300f"

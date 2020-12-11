5a11139619814db1a0ec8375eebecc9bcf3d4687
glauroqj <glauro.juliani@hotmail.com>
2020-12-11 18:33:50 -0300

    add: access room svg

--- src/pages/Home/Home.style.js
+++ src/pages/Home/Home.style.js
@@ -39,3 +39,17 @@ export const HomeLogin = styled.div`
     margin-bottom: 15px; 
   }
 `
+
+export const HomeAccessRoom = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  max-width: 320px;
+
+  > svg {
+    width: 100%;
+    height: auto;
+    margin-bottom: 15px; 
+  }
+`
--- src/pages/Home/_accessRoom.js
+++ src/pages/Home/_accessRoom.js
@@ -5,21 +5,24 @@ import Button from '@material-ui/core/Button'
 /** style */
 import * as El from './Home.style'
 /** svg */
-import { ReactComponent as ScrumIcon }  from 'assets/images/scrum.svg'
+import { ReactComponent as VoteIcon }  from 'assets/images/voting.svg'
 /** icons */
 import MeetingRoomIcon from '@material-ui/icons/MeetingRoom'
 
 const AccessRoom = ({link}) => (
-  <Link to={`${link}`}>
-    <Button
-      variant='contained'
-      color='secondary'
-      size='large'
-      fullWidth
-    >
-      go to {`${link}`} <MeetingRoomIcon />
-    </Button>
-  </Link>
+  <El.HomeAccessRoom>
+    <VoteIcon />
+    <Link to={`${link}`}>
+      <Button
+        variant='contained'
+        color='secondary'
+        size='large'
+        fullWidth
+      >
+        go to {`${link}`} <MeetingRoomIcon />
+      </Button>
+    </Link>
+  </El.HomeAccessRoom>
 )
 
 export default AccessRoom
